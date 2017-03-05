e2d8855242f11d381fbca84bea6e4e74a50fe8f3
Diego Pereira <rockin.jack@gmail.com>
2017-03-04 22:26:48 -0300

    fix: generating valid json output again
    slightly less hackish

--- src/jx/__init__.py
+++ src/jx/__init__.py
@@ -45,13 +45,13 @@ class TokenParser:
     def get_parser(self, token):
         return {
             self.tokens.OBJECT: JsonParser(),
-            self.tokens.OBJ_OPEN: ConstantFragment('{'),
+            self.tokens.OBJ_OPEN: ConstantFragment('{', inline=True),
             self.tokens.OBJ_CLOSE: ConstantFragment('}'),
             self.tokens.LIST: DefaultParser(),
-            self.tokens.LIST_OPEN: ConstantFragment('['),
+            self.tokens.LIST_OPEN: ConstantFragment('[', inline=True),
             self.tokens.LIST_CLOSE: ConstantFragment(']'),
             self.tokens.KEY_AND_VALUE: KeyAndValueParser(),
-            self.tokens.KEY_AND_VALUE_SEPARATOR: ConstantFragment(': '),
+            self.tokens.KEY_AND_VALUE_SEPARATOR: ConstantFragment(': ', inline=True),
             self.tokens.KEY: DefaultParser(),
             self.tokens.VALUE: DefaultParser(),
         }[token]
@@ -102,7 +102,7 @@ class TokenParser:
 
     def parse_before(self, instance, ctx):
         line, level, attribute_index, label, inline = self._expand_ctx(ctx)
-        fragment = ',' if attribute_index  else ''
+        fragment = ',' if attribute_index  and not inline else ''
         fragment += '' if inline else '\n' + ('\t' * level)
         return fragment
 
@@ -116,22 +116,36 @@ class TokenParser:
 
 class DefaultParser(TokenParser):
 
+    def parse_before(self, key, ctx):
+        return ''
+
     def parse_token(self, key, ctx):
         return json.dumps(key)
 
+    def line_number(self, instance, ctx):
+        return ctx['line']
+
 class ConstantFragment(TokenParser):
 
-    def __init__(self, fragment):
+    def __init__(self, fragment, inline=False):
         self._fragment = fragment
+        self._inline = inline
 
     def parse_before(self, instance, ctx):
-        return ''
+        self._inline_ctx = ctx['inline']
+        ctx['inline'] = self._inline
+        return super().parse_before(instance, ctx)
 
     def line_number(self, instance, ctx):
         return ctx['line']
 
     def parse_token(self, instance, ctx):
         return self._fragment
+
+    def parse_after(self, instance, ctx):
+        ctx['inline'] = self._inline_ctx
+        return super().parse_after(instance, ctx)
+        
 
 class KeyAndValueParser(TokenParser):
 
@@ -146,10 +160,10 @@ class KeyAndValueParser(TokenParser):
             token = self.tokens.LIST
         else:
             token = self.tokens.VALUE
-
+        
         value_fragment, ctx = self.emit(value, ctx, token)
 
-        return '"{k}"{s} "{v}" '.format(
+        return '{k}{s} {v}'.format(
             k=key_fragment,
             s=key_value_separator_fragment,
             v=value_fragment
@@ -167,12 +181,16 @@ class JsonParser(TokenParser):
         return '' 
 
     def parse_token(self, instance, ctx):
-        fragment, ctx = self.emit(instance, ctx, self.tokens.OBJ_OPEN)
+        fragment, child_ctx = self.emit(instance, ctx, self.tokens.OBJ_OPEN)
+        ctx['level'] += 1
 
         for attribute_index, key_and_value in enumerate(instance.items()):
             ctx['attribute_index'] = attribute_index
             key_and_value_fragment, ctx = self.emit(key_and_value, ctx, self.tokens.KEY_AND_VALUE)
             fragment += key_and_value_fragment
+
+        ctx['level'] -= 1
+        ctx['attribute_index'] = 0
 
         closure_fragment, ctx = self.emit(instance, ctx, self.tokens.OBJ_CLOSE)
 
@@ -180,36 +198,6 @@ class JsonParser(TokenParser):
 
     def print_object(self, instance, ctx=None):
         return self.parse_token(instance, ctx if ctx else self._new_ctx(object_index=self.object_index))
-#        line, level, attribute_index, label, inline = self._expand_ctx(ctx)
-#        buffer = ''
-#        b,line = self._buffer_add(instance, "{", line, inline=True, i=attribute_index, label=label)
-#        buffer +=  b
-#
-#        for i, (attr, value) in enumerate(instance.items()):
-#            b, line = self._buffer_add(instance, , line, level + 1, i, label=label)
-#            buffer += '{k}: '.format(k=json.dumps(attr))
-#            if isinstance(value, dict):
-#                child_ctx = self._new_ctx(
-#                    line, level + 1, i, label + [attr]
-#                )
-#                o, line = self.print_object(value, child_ctx)
-#            elif isinstance(value, list):
-#                # TODO: support lists
-#                child_ctx = self._new_ctx(
-#                    line, level + 1, i, label + [attr]
-#                )
-#                o, line = self.print_object({}, child_ctx)
-#            else:
-#                o = json.dumps(value)
-#
-#            b, line = self._buffer_add(instance, o, line, level + 1, i=0, inline=True, label=label)
-#            buffer += b
-#
-#        b, line = self._buffer_add(instance, "}", line, level, 0, inline=(len(instance)==0), exc=True, label=label)
-#        buffer += b
-#
-#        return buffer, line
-
 
 class Jx:
 
@@ -243,16 +231,18 @@ class Jx:
         self.index = jp.object_index
         seen_objects = set()
 
-        for i, l in enumerate(self.buffer[0].split('\n')):
-            object_rep = json.dumps(self.index[i][1], sort_keys=True)
-            object_name = self.index[i][-1][-1]
+        with open('/tmp/original.json', 'w+') as f:
+            f.write(json.dumps(self.data, indent=1))
 
-            self._window.addstr('\r{:>15}> {}'.format(object_name, l))
-            if object_rep not in seen_objects:
-                seen_objects.add(object_rep)
-                self._window.addstr(' ')
-                self._window.addstr('-', curses.color_pair(1))
+        with open('/tmp/jx.json', 'w+') as f:
+            f.write(self.buffer)
 
+
+        for i, l in enumerate(self.buffer.split('\n')):
+            #object_rep = json.dumps(self.index[i][1], sort_keys=True)
+            #object_name = self.index[i][-1][-1]
+
+            self._window.addstr('\r{}'.format(l))
             self._window.addstr('\n')
 
     def _trap_events(self):
